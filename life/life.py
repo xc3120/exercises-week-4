838f8ea2d7d1f40817fa00e373a1dfb089e32285
xc3120 <99423066+xc3120@users.noreply.github.com>
2022-02-11 11:10:54 +0000

    Update life.py

--- life/life.py
+++ life/life.py
@@ -1,13 +1,11 @@
+"""life module."""
 import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
-blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]])
+blinker = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -48,30 +46,79 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
-    
+    """class Game."""
+
+    def __init__(self, size):
+        """Initialise."""
+        self.board = np.zeros((size, size))
+
     def play(self):
+        """Play."""
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+        """Move."""
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (
+                    neighbourcount[i, j] == 3 or (
+                        neighbourcount[i, j] == 2 and self.board[i, j])) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
+        """Set item."""
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        """Show."""
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
+
+    def insert(self, pattern, pair):
+        """Insert."""
+        self.board[pair[0]-len(
+            pattern.grid[0]) // 2: pair[0] + len(pattern.grid[0]) // 2 + 1,
+            pair[1]-len(pattern.grid[0]) // 2: pair[1] + len(
+                pattern.grid[0]) // 2 + 1] = pattern.grid
+
+
+class Pattern:
+    """Pattern."""
+
+    def __init__(self, grid):
+        """Initialise it."""
+        self.grid = grid
+
+    def flip_vertical(self):
+        """Flip vertically."""
+        return Pattern(self.grid[::-1, :])
+
+    def flip_horizontal(self):
+        """Flip horizontally."""
+        return Pattern(self.grid[:, ::-1])
+
+    def flip_diag(self):
+        """Flip diagonally."""
+        return Pattern(np.transpose(self.grid))
+
+    def rotate(self, n):
+        """Rotate."""
+        self.n = n
+        if n % 4 == 0:
+            return Pattern(self.grid)
+        elif n % 4 == 1:
+            return Pattern(self.flip_diag().flip_vertical().grid)
+        elif n % 4 == 2:
+            return Pattern(self.flip_horizontal().flip_vertical().grid)
+        elif n % 4 == 3:
+            return Pattern(self.flip_diag().flip_horizontal().grid)
